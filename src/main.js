fb5dfa8abe846b61fe50fc8cbaf5c18d88050db8
Fernando-Ibarra <fi94457@gmail.com>
2022-08-23 12:11:09 -0600

    [ADD] Axios Movie and Categories Preview

--- src/main.js
+++ src/main.js
@@ -1,3 +1,54 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
 
+async function getTrendingMoviesPreview() {
+    const { data } = await api('trending/movie/week');
+    const movies = data.results;
 
-console.log('Hello');
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+async function getCategoriesPreview() {
+    const { data } = await api('genre/movie/list');
+    const categories = data.genres;
+
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTittle = document.createElement('h3');
+        categoryTittle.classList.add('category-title');
+        categoryTittle.setAttribute('id', 'id' + category.id);
+
+        const categoryTitleText = document.createTextNode(category.name) 
+
+        categoryTittle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTittle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
